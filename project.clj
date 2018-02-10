8c7562ad6f34c5380a4154f33fa947a68001ca54
em7 <mm007.emko@gmail.com>
2018-02-10 09:43:38 +0100

    Added build profile for macosx

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :win64 {:dependencies [[cas-sluzebka/swt.win64 "4.7.2"]]}
-             :linux-gtk64 {:dependencies [[cas-sluzebka/swt.linux.gtk64 "4.7.2"]]}}
+             :linux-gtk64 {:dependencies [[cas-sluzebka/swt.linux.gtk64 "4.7.2"]]}
+             :macosx-cocoa64 {:dependencies [[cas-sluzebka/swt.macosx.cocoa64 "4.7.2"]]}}
   :pom-addition [:properties
                  [:maven.compiler.source "1.8"]
                  [:maven.compiler.target "1.8"]]
